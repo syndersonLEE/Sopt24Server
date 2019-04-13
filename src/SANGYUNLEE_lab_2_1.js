39a2fc303b264a68a46b96d70512645461f12098
Unknown <winnery93@gmail.com>
2019-04-13 17:24:25 +0900

    2차 세미나 수정

--- src/SANGYUNLEE_lab_2_1.js
+++ src/SANGYUNLEE_lab_2_1.js
@@ -1,6 +1,8 @@
 
 const http = require('http');
 const url = require('url');
+const querystring = require('querystring');
+const crypto = require('crypto');
 
 const server = http.createServer((request, response) => {
     const urlparse = url.parse(request.url);
@@ -11,12 +13,14 @@ const server = http.createServer((request, response) => {
             console.log(err);
         } else {
             const salt = buf.toString('base64');
-            crypto.pdkdf2(str, salt, 10, 32, 'SHA512', (err, result) => {
+            console.log(`salt : ${salt}`);
+            crypto.pbkdf2(str, salt, 10, 32, 'SHA512', (err, result) => {
                 const hashedStr = result.toString('base64');
                 console.log(`hashedStr : ${hashedStr}`);
-                request.writeHead(200, {'Content-Type' : 'appication/json' });
-                request.write(hashedStr);
-                console.log(hashedStr);
+                
+                response.writeHead(200, {'Content-Type' : 'application/json'});
+                response.write(hashedStr);
+                response.end();
             })
         }
     })
